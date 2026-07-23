628861fd6d380d9b764f8dd9920f70969347653d
kezhilan <kezhilan@charlotte.edu>
2026-07-22 21:02:41 -0400

    CRUD added for recipes, resources, and sandwiches according to the prev changes

---
+++ api/controllers/recipes.py
@@ -0,0 +1,82 @@
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, status, Response
+from sqlalchemy.exc import SQLAlchemyError
+from ..models import recipes as model
+
+
+def create(db: Session, request):
+    new_item = model.Recipe(
+        sandwich_id=request.sandwich_id,
+        resource_id=request.resource_id,
+        amount=request.amount
+    )
+
+    try:
+        db.add(new_item)
+        db.commit()
+        db.refresh(new_item)
+
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=error
+        )
+
+    return new_item
+
+
+def read_all(db: Session):
+    return db.query(model.Recipe).all()
+
+
+def read_one(db: Session, item_id):
+    item = db.query(model.Recipe).filter(
+        model.Recipe.id == item_id
+    ).first()
+
+    if not item:
+        raise HTTPException(
+            status_code=404,
+            detail="Id not found!"
+        )
+
+    return item
+
+
+def update(db: Session, item_id, request):
+    item = db.query(model.Recipe).filter(
+        model.Recipe.id == item_id
+    )
+
+    if not item.first():
+        raise HTTPException(
+            status_code=404,
+            detail="Id not found!"
+        )
+
+    item.update(
+        request.dict(exclude_unset=True),
+        synchronize_session=False
+    )
+
+    db.commit()
+
+    return item.first()
+
+
+def delete(db: Session, item_id):
+    item = db.query(model.Recipe).filter(
+        model.Recipe.id == item_id
+    )
+
+    if not item.first():
+        raise HTTPException(
+            status_code=404,
+            detail="Id not found!"
+        )
+
+    item.delete(synchronize_session=False)
+    db.commit()
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
