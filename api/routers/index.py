87af3fb0dfc2d48e39d66cc675f9fef3b653e4e5
kezhilan <kezhilan@charlotte.edu>
2026-07-22 21:05:34 -0400

    index.py fix

--- api/routers/index.py
+++ api/routers/index.py
@@ -1,7 +1,10 @@
-from . import orders, order_details, customers
+from . import orders, order_details, customers, sandwiches, recipes, resources
 
 
 def load_routes(app):
     app.include_router(orders.router)
     app.include_router(order_details.router)
     app.include_router(customers.router)
+    app.include_router(sandwiches.router)
+    app.include_router(resources.router)
+    app.include_router(recipes.router)
